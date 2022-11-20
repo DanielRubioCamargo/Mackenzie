6db437efd40303b766d88199ad0de87695246f17
Daniel Camargo <drubiocamargo@gmail.com>
2022-11-19 21:41:31 -0300

    Finalizações!

--- Primeiro Semestre/Algoritmo e Programação/urna.py
+++ Primeiro Semestre/Algoritmo e Programação/urna.py
@@ -11,7 +11,7 @@ def mostrarMenu():
     ''')
 
 # cadastra um candidato e insere uma lista com seus dados (nome, numero, partido, cargo e quantidade de votos) em uma lista global l
-def cadastrarCandidato(l): 
+def cadastrarCandidato(l, lpartidos): 
 
     while True:
 
@@ -27,6 +27,15 @@ def cadastrarCandidato(l):
         lAux.append(cargo)
         lAux.append(0)
         l.append(lAux)
+
+        for i, p in enumerate(lpartidos): # cria uma nova posição na lista de partidos (se não existir)
+            if p[0] == partido:
+                break
+            if i == len(lpartidos)-1:
+                auxPartidos = list()
+                auxPartidos.append(partido)
+                auxPartidos.append(0)
+                lpartidos.append(auxPartidos)
 
         print()
         op = input("Deseja continuar? [sim/nao] : ").lower()
@@ -45,12 +54,25 @@ def cadastrarEleitor(l):
         nome = input("Insira seu nome : ").title()
         cpf = input("Insira seu CPF : ")
 
-        lAux = list()
-        lAux.append(nome)
-        lAux.append(cpf)
-        for i in range(3):
-            lAux.append(False)
-        l.append(lAux)
+        podeDarContinue = False
+
+        for i in range(len(l)): # checa se o cpf já existe
+            if l[i][1] == cpf:
+                print()
+                print("\033[0;31mNúmero de CPF existente!\033[m")
+                print()
+                podeDarContinue = True
+                break
+            if i == len(l)-1:
+                lAux = list()
+                lAux.append(nome)
+                lAux.append(cpf)
+                for i in range(3):
+                    lAux.append(False)
+                l.append(lAux)
+
+        if podeDarContinue:
+            continue
 
         print()
         op = input("Deseja continuar? [sim/nao] : ").lower()
@@ -61,11 +83,11 @@ def cadastrarEleitor(l):
         break
 
 ''' função para votação, parametros : lista dos candidadtos(l), lista dos eleitores(e), lista de votos brancos(b),
-lista de votos nulos(n), total de votos(tv)
+lista de votos nulos(n), total de votos(tv), lista de partidos(lp)
 '''
-def votar(l, e, b, n, tv):
+def votar(l, e, b, n, tv, lp):
 
-    for el in range(len(e)): # percorre a lista de candidatos
+    for el in range(1,len(e)): # percorre a lista de eleitores
         print()
         if len(l) != 0: # verifica se há candidatos para serem votados
             listaCargos = ["PREFEITO","GOVERNADOR","PRESIDENTE"]
@@ -104,6 +126,9 @@ def votar(l, e, b, n, tv):
                                     op2 = input("\033[0;33mCandidato : {} - {} | Confirma? [sim/nao] : \033[m".format(l[k][0],l[k][2])).lower()
                                     print() # op2 mostra o candidato segundo a posição do for e quer confirmar a escolha
                                     if op2 == "sim":
+                                        for a in range(len(lp)):
+                                            if lp[a][0] == l[k][2]:
+                                                lp[a][1] += 1
                                         l[k][4] += 1 # mais um voto para este candidato
                                         tv[i] += 1 # total de votos incrementado
                                         e[el][i+2] = True # eleitor votou
@@ -131,7 +156,7 @@ def separarCargos(l, pref, gov, pres): # adiciona cada candidato em sua respecti
         else:
             pres.append(i)
 
-def ordenarLista(lista): # ordenar por quantidade de votos
+def ordenarListaVotosCandidatos(lista): # ordenar por quantidade de votos
 
     for i in range(len(lista)-1):
         for j in range(i+1, len(lista)):
@@ -147,9 +172,11 @@ def criarMenuParaApuracao(cargo, lista, vb, vn, pos, tv): # cria a interface das
         print("="*v+" "+cargo+" "+"="*v)
         print(("Vencedor : {}".format(lista[0][0])).center(2*v+len(cargo)))
         print("="*(v+1)+"="*len(cargo)+"="*(v+1))
+
         for i, c in enumerate(lista):
             print("{}º : {} ( {} ) | {} VOTO(S) |".format(i+1, c[0], c[2], c[4]))
         print("="*(v+1)+"="*len(cargo)+"="*(v+1))
+        
         print()
         print("Total de votos : {}\n".format(tv[pos]))
 
@@ -164,26 +191,54 @@ def criarMenuParaApuracao(cargo, lista, vb, vn, pos, tv): # cria a interface das
 
 def apurarResultados(pref, gov, pres, vb, vn, tv):
 
-    ordenarLista(pref)
-    ordenarLista(gov)
-    ordenarLista(pres)
+    ordenarListaVotosCandidatos(pref)
+    ordenarListaVotosCandidatos(gov)
+    ordenarListaVotosCandidatos(pres)
 
     criarMenuParaApuracao("PREFEITO",pref, vb, vn, 0, tv)
     criarMenuParaApuracao("GOVERNADOR",gov, vb, vn, 1, tv)
     criarMenuParaApuracao("PRESIDENTE",pres, vb, vn, 2, tv)
 
+def retornarPartidoMaisVotadoEMenosVotado(lp):
+    maior = 0
+    maiorPartido = ''
+    menor = 0
+    menorPartido = ''
+    for i in range(1,len(lp)):
+        if i == 1:
+            menor = lp[i][1]
+            menorPartido = lp[i][0]
+        if lp[i][1] > maior:
+            maior = lp[i][1]
+            maiorPartido = lp[i][0]
+        elif lp[i][1] < menor:
+            menor = lp[i][1]
+            menorPartido = lp[i][0]
+    return maiorPartido, menorPartido # retorna o partido com mais votos e o com menos
 
-def mostrarReE():
-    pass
+def mostrarEleitores(el): # lista de todos os eleitores
+    print("Lista de eleitores : ")
+    for i in range(1,len(el)):
+        print("-> ",el[i][0])
+    print()
+
+def mostrarReE(lp, el): # relatório e estatisticas
+    mostrarEleitores(el)
+    maiorPartido, menorPartido = retornarPartidoMaisVotadoEMenosVotado(lp)
+    print("Partido mais votado : {}".format(maiorPartido))
+    print("Partido menos votado : {}".format(menorPartido))
+    print()
 
 candidatos = list()
-eleitores = list()
+eleitores = [['','',False,False,False]]
 votosBrancos = [0,0,0]
 votosNulos = [0,0,0]
 
 prefeitos = list()
 governadores = list()
 presidentes = list()
+
+partidos = [['',0]]
 
 totalVotos = [0,0,0]
 
@@ -197,7 +252,7 @@ while True:
 
     if digito == 1:
 
-        cadastrarCandidato(candidatos)
+        cadastrarCandidato(candidatos, partidos)
 
     elif digito == 2:
 
@@ -205,7 +260,7 @@ while True:
 
     elif digito == 3:
 
-        votar(candidatos, eleitores, votosBrancos, votosNulos, totalVotos)
+        votar(candidatos, eleitores, votosBrancos, votosNulos, totalVotos, partidos)
 
     elif digito == 4:
         
@@ -213,7 +268,7 @@ while True:
         apurarResultados(prefeitos, governadores, presidentes, votosBrancos, votosNulos, totalVotos)
 
     elif digito == 5:
-        pass
+        mostrarReE(partidos, eleitores)
 
     elif digito == 6:
         print("Programa finalizado!\n")
