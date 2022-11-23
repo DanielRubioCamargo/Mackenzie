60fbce34381ecb052b42a63d428326b3bdf6f6cc
Daniel Camargo <drubiocamargo@gmail.com>
2022-11-22 21:22:24 -0300

    Update!

--- Primeiro Semestre/Circuitos EeE/separador.c
+++ Primeiro Semestre/Circuitos EeE/separador.c
@@ -1,5 +1,4 @@
-#include <Servo.h>
-Servo servoid1;
+#include Servo servoid1;
 Servo servoid2;
 
 const int s0 = 8;
@@ -19,60 +18,64 @@ int blue = 0;
 
 void setup()
 {
-    servoid1.attach(3);
-    servoid2.attach(4);
-    pinMode(s0, OUTPUT);
-    pinMode(s1, OUTPUT);
-    pinMode(s2, OUTPUT);
-    pinMode(s3, OUTPUT);
-    pinMode(out, INPUT);
-    pinMode(pinoledverm, OUTPUT);
-    pinMode(pinoledazul, OUTPUT);
-    Serial.begin(9600);
-    digitalWrite(s0, HIGH);
-    digitalWrite(s1, LOW);
+  servoid1.attach(3);
+  servoid2.attach(4);
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(out, INPUT);
+  pinMode(pinoledverm, OUTPUT);
+  pinMode(pinoledazul, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(s0, HIGH);
+  digitalWrite(s1, LOW);
 }
 
 void loop()
 {
-	
-  //Detecta a cor
-    color();
 
-  //Verifica se a cor vermelha foi detectada
-    if (red < blue && red < green && red < 100)
-    {
-        Serial.println("Vermelho");
-        digitalWrite(pinoledverm, HIGH); 
-        digitalWrite(pinoledazul, LOW);
-        servoid1.write(180);
-        delay(2000);
-        servoid2.write(180); 
-        delay(5000);
-        servoid1.write(0);
-        servoid2.write(0);
-    } 
-  //Delay para apagar os leds e reiniciar o processo
-    else{
-        delay(1000);
-        digitalWrite(pinoledverm, LOW);
-        digitalWrite(pinoledazul, LOW);
-        servoid1.write(0);
-        servoid2.write(0);
-    }
+//Detecta a cor
+color();
+servoid1.write(-90);
+digitalWrite(pinoledverm, LOW);
+digitalWrite(pinoledazul, LOW);
+
+//Verifica se a cor vermelha foi detectada
+if (red < blue && red < green && red < 100)
+{
+  Serial.println("Vermelho");
+  digitalWrite(pinoledverm, HIGH);
+  digitalWrite(pinoledazul, LOW);
+  servoid1.write(90);
+  servoid2.write(90);
+  delay(3000);
+  servoid1.write(-90);
+  servoid2.write(0);
+  digitalWrite(pinoledverm, LOW);
+}
+//Delay para apagar os leds e reiniciar o processo
+else if (green < blue && green < blue && green < 100){
+    digitalWrite(pinoledverm, LOW);
+    digitalWrite(pinoledazul, HIGH);
+    servoid1.write(90);
+    delay(2000);
+    servoid1.write(-90);
+    digitalWrite(pinoledazul, LOW);
+  }
 }
 
 void color()
 {
   //Rotina que le o valor das cores
-    digitalWrite(s2, LOW);
-    digitalWrite(s3, LOW);
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, LOW);
   //count OUT, pRed, RED
-    red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-    digitalWrite(s3, HIGH);
+  red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+  digitalWrite(s3, HIGH);
   //count OUT, pBLUE, BLUE
-    blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-    digitalWrite(s2, HIGH);
+  blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+  digitalWrite(s2, HIGH);
   //count OUT, pGreen, GREEN
-    green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+  green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
 }
